7e5c945654d01cdef40e5c914bdc13dffd9082a9
Rizky Fauzi Ilmi <104153142+RizkyFauziIlmi@users.noreply.github.com>
2023-01-20 01:03:53 +0700

    fixed force close 3.0.0 version

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId
+  apiKey: process.env.apiKey||"AIzaSyAb6Umkkb6M-nPKZ7QY5kdCrUUZSmQXJvc",
+  authDomain: process.env.authDomain||"jozu-81b35.firebaseapp.com",
+  projectId: process.env.projectId||"jozu-81b35",
+  messagingSenderId: process.env.messagingSenderId||"348212962396",
+  appId: process.env.appId||"1:348212962396:web:f39a8aa0693fe37f633299",
+  measurementId: process.env.measurementId||"G-D11JB605C8"
 };
 
 // Initialize Firebase
--- metro.config.js
+++
@@ -1,6 +0,0 @@
-const { getDefaultConfig } = require('@expo/metro-config');
-
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.assetExts.push('cjs');
-
-module.exports = defaultConfig;
